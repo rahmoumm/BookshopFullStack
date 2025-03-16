1057c9c35bf3855a0e43fec5112c83993d2ae1d8
Mohammed Mourad Rahmouni <rahmounimourad01@gmail.com>
2025-03-16 23:59:08 +0100

    supression des fichiers sql

--- bookshop_backend/demo/src/main/resources/data.sql
+++ bookshop_backend/demo/src/main/resources/data.sql
@@ -1,21 +1,21 @@
 INSERT INTO Book (book_id, name, rating) VALUES (11, 'gambler', 4.5) ON CONFLICT (book_id) DO NOTHING;
 INSERT INTO Book (book_id, name, rating) VALUES (12, 'hz', 3.5)ON CONFLICT (book_id) DO NOTHING;
 INSERT INTO Book (book_id, name, rating) VALUES (13, 'gamazvbler', 1.9)ON CONFLICT (book_id) DO NOTHING;
---
----- password est abc
---INSERT INTO MY_USERS (user_id, first_name, last_name, email, password)
---    VALUES (11, 'Mourad', 'Rahmouoni', 'm@gmail.com','$2y$10$/xdmnLZAh1IW192a1CyWO.HWq4OAZQIv6UNxk1sSkZkNEk/9uTCVW')
---    ON CONFLICT (user_id) DO NOTHING;
---
-----password est def
---INSERT INTO MY_USERS (user_id, first_name, last_name, email, password)
---    VALUES (12, 'Karim', 'Rahmouoni', 'k@gmail.com','$2y$10$kcIIpdu3O8q2JAL6jXSZb.byOz4y/Tmdq28456yulnsPa7fo.78wm')
---    ON CONFLICT (user_id) DO NOTHING;
---
----- password est ghi
---INSERT INTO MY_USERS (user_id, first_name, last_name, email, password)
---    VALUES (13, 'Hamid', 'Rahmouoni', 'h@gmail.com','$2y$10$JwJSxuk06GFJo7v5lmmq4ONdYRhrodVhjVZbbtXp1d/Q23ZBkq9He')
---    ON CONFLICT (user_id) DO NOTHING;
+
+-- password est abc
+INSERT INTO MY_USERS (user_id, first_name, last_name, email, password)
+    VALUES (11, 'Mourad', 'Rahmouoni', 'm@gmail.com','$2y$10$/xdmnLZAh1IW192a1CyWO.HWq4OAZQIv6UNxk1sSkZkNEk/9uTCVW')
+    ON CONFLICT (user_id) DO NOTHING;
+
+-- password est def
+INSERT INTO MY_USERS (user_id, first_name, last_name, email, password)
+    VALUES (12, 'Karim', 'Rahmouoni', 'k@gmail.com','$2y$10$kcIIpdu3O8q2JAL6jXSZb.byOz4y/Tmdq28456yulnsPa7fo.78wm')
+    ON CONFLICT (user_id) DO NOTHING;
+
+-- password est ghi
+INSERT INTO MY_USERS (user_id, first_name, last_name, email, password)
+    VALUES (13, 'Hamid', 'Rahmouoni', 'h@gmail.com','$2y$10$JwJSxuk06GFJo7v5lmmq4ONdYRhrodVhjVZbbtXp1d/Q23ZBkq9He')
+    ON CONFLICT (user_id) DO NOTHING;
 --
 --INSERT INTO STOCK (user_id, book_id, available_quantity, price) VALUES(11, 11, 10, 15.0) ON CONFLICT (book_id, user_id) DO NOTHING;
 --INSERT INTO STOCK (user_id, book_id, available_quantity, price) VALUES(11, 12, 15, 25.99) ON CONFLICT (book_id, user_id) DO NOTHING;
