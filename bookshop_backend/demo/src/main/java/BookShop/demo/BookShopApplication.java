ae45e3bcbe6227f40bda81d27f9e32d46fb7445b
Mohammed Mourad Rahmouni <rahmounimourad01@gmail.com>
2025-03-21 15:53:10 +0100

    Usage des variables d'environnement au lieu de mettre le username et password directement pour des soucis de sécurité

--- bookshop_backend/demo/src/main/java/BookShop/demo/BookShopApplication.java
+++ bookshop_backend/demo/src/main/java/BookShop/demo/BookShopApplication.java
@@ -2,11 +2,18 @@ package BookShop.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BookShopApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("./bookshop_backend/demo/").load();
+
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_LINK&", dotenv.get("DB_LINK"));
+
 		SpringApplication.run(BookShopApplication.class, args);
 	}
 
